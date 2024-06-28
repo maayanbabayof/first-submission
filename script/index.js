dee7e4a704026c637444d99c1c33188fcbde7be1
nir <niritah98@gmail.com>
2024-06-28 18:52:01 +0300

    update few fiels look, also change in index the view of the opptuninty to be with json files

--- script/index.js
+++ script/index.js
@@ -2,42 +2,47 @@ document.addEventListener("DOMContentLoaded", () => {
   const opportunitiesList =
     document.getElementsByClassName("opportunities-list");
 
-  opportunities.cards.forEach((card) => {
-    const opportunityDiv = document.createElement("div");
-    opportunityDiv.className = "opportunity-div";
+  fetch("data/opportunities.json")
+    .then((response) => response.json())
+    .then((data) => {
+      data.cards.forEach((card) => {
+        const opportunityDiv = document.createElement("div");
+        opportunityDiv.className = "opportunity-div";
 
-    const img = document.createElement("img");
-    img.alt = "image";
-    img.src = "images/" + card.image;
+        const img = document.createElement("img");
+        img.alt = "image";
+        img.src = "images/" + card.image;
 
-    const infoDiv = document.createElement("div");
+        const infoDiv = document.createElement("div");
 
-    const title = document.createElement("p");
-    title.className = "title";
-    title.textContent = card.title;
+        const title = document.createElement("p");
+        title.className = "title";
+        title.textContent = card.title;
 
-    const description = document.createElement("p");
-    description.className = "description";
-    description.innerHTML = `
+        const description = document.createElement("p");
+        description.className = "description";
+        description.innerHTML = `
       Region: ${card.region} <br>
       City: ${card.city} <br>
       Rating: ${card.rating} <br>
       Date: ${card.date}
     `;
 
-    const deleteButton = document.createElement("button");
-    deleteButton.textContent = "Delete";
-    deleteButton.classList.add("delete-button");
-    deleteButton.addEventListener("click", () => {
-      opportunitiesList[0].removeChild(opportunityDiv);
-    });
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.classList.add("delete-button");
+        deleteButton.addEventListener("click", () => {
+          opportunitiesList[0].removeChild(opportunityDiv);
+        });
 
-    infoDiv.appendChild(title);
-    infoDiv.appendChild(description);
-    infoDiv.appendChild(deleteButton);
-    opportunityDiv.appendChild(img);
-    opportunityDiv.appendChild(infoDiv);
+        infoDiv.appendChild(title);
+        infoDiv.appendChild(description);
+        infoDiv.appendChild(deleteButton);
+        opportunityDiv.appendChild(img);
+        opportunityDiv.appendChild(infoDiv);
 
-    opportunitiesList[0].appendChild(opportunityDiv);
-  });
+        opportunitiesList[0].appendChild(opportunityDiv);
+      });
+    })
+    .catch((error) => console.error("Error fetching data:", error));
 });
