d198771a0a84afc155b79d13a80cf67f18d1a364
nir <niritah98@gmail.com>
2024-07-26 14:00:00 +0300

    update for list look

--- client/js/list.js
+++ client/js/list.js
@@ -1,30 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("http://localhost:3000/api/opportunities/all")
-        .then(response => response.json())
-        .then(data => {
-            setTitle("Opportunities");
-            createAndAppendCards(data);
-        })
-        .catch(error => console.error("Error fetching data:", error));
+  fetch("http://localhost:3000/api/opportunities/all")
+    .then((response) => response.json())
+    .then((data) => {
+      setTitle("Opportunities");
+      createAndAppendCards(data);
+    })
+    .catch((error) => console.error("Error fetching data:", error));
 });
 
 function setTitle(title) {
-    document.getElementById("pageTitle").innerText = title;
+  document.getElementById("pageTitle").innerText = title;
 }
 
 function createAndAppendCards(cards) {
-    const cardsContainer = document.querySelector("#cardsContainer .row");
+  const cardsContainer = document.querySelector("#cardsContainer .cardsGrid");
 
-    cards.forEach(card => {
-        const cardElement = document.createElement("div");
-        cardElement.className = "card mb-3";
-        cardElement.style.maxWidth = "540px";
-        cardElement.innerHTML = `
-            <div class="row g-0">
-                <div class="col-md-4">
+  cards.forEach((card) => {
+    const cardElement = document.createElement("div");
+    cardElement.className = "card mb-3 card-width";
+
+    cardElement.innerHTML = `
+            <div class="col g-0">
+                <div class="col">
                     <img src="${card.img}" class="img-fluid rounded-start" alt="${card.title}">
                 </div>
-                <div class="col-md-8">
+                <div class="col">
                     <div class="card-body">
                         <h5 class="card-title">${card.title}</h5>
                         <p class="card-text">${card.region}, ${card.city}</p>
@@ -35,10 +35,10 @@ function createAndAppendCards(cards) {
             </div>
         `;
 
-        cardElement.addEventListener("click", () => {
-            window.location.href = `object.html?id=${card.opportunity}`;
-        });
-
-        cardsContainer.appendChild(cardElement);
+    cardElement.addEventListener("click", () => {
+      window.location.href = `object.html?id=${card.opportunity}`;
     });
+
+    cardsContainer.appendChild(cardElement);
+  });
 }
